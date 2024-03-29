acf998e2320adb41f1066b0653d29c3a3f7c991f
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-03-29 09:28:37 +0300

    eventeListener

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+
 function addingEventListener() {
+    let button = document.getElementById('button');
+    button.addEventListener('click', function(){
+        alert('true')
+    })
+
 }
+console. log(addingEventListener);
+
+
+
